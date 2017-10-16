4810ed2cd3fe5bc25ac44dc05ea7baf125f6bcce
David Grougan <ddogclaw@gmail.com>
2017-10-16 10:44:55 -0400

    Debugging

--- Assets/Scripts/CollidableObject.cs
+++ Assets/Scripts/CollidableObject.cs
@@ -38,15 +38,17 @@ public class CollidableObject : MonoBehaviour {
                 CollisionDirection direction = GetDirection(bounds, otherCollider.bounds);
                 // "Bump" out of the collider we're encountering.
                 BumpOut(otherCollider.bounds,direction);
+                Debug.Log("Bumped out collision between: " + gameObject.name + " and " + colObject.name);
                 // Apply force to other object and a normal force to ourselves.
                 Vector3 force = new Vector3();
                 // Only apply the force for the direction of the collision. (Because who needs friction?)
-                if (direction == CollisionDirection.Left || direction == CollisionDirection.Right)
+                if (!physics.LockPositionX && (direction == CollisionDirection.Left || direction == CollisionDirection.Right))
                     force.x = physics.Mass * physics.Velocity.x / Time.deltaTime;
-                if (direction == CollisionDirection.Up || direction == CollisionDirection.Down)
+                if (!physics.LockPositionY && (direction == CollisionDirection.Up || direction == CollisionDirection.Down))
                     force.y = physics.Mass * physics.Velocity.y / Time.deltaTime;
-                if (direction == CollisionDirection.Front || direction == CollisionDirection.Back)
+                if (!physics.LockPositionZ && (direction == CollisionDirection.Front || direction == CollisionDirection.Back))
                     force.z = physics.Mass * physics.Velocity.z / Time.deltaTime;
+                Debug.Log("Applied force: " + force);
                 colObject.GetComponent<PhysicsObject>().AddForceAndNormal(force,physics);
             }
         }
